8badec8da70715b05961aae06e160bed1ba09949
Анастасия Мершавка <mershavkaa@yandex.ru>
2021-03-31 20:15:49 +0300

    Схема установки

--- first_step.py
+++ first_step.py
@@ -2,6 +2,7 @@ import csv
 import pandas
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 from scipy.optimize import curve_fit
  
 # with open('TGS2611_TGS2600_Calibration.csv', encoding='utf-8') as File:
@@ -18,22 +19,32 @@ from scipy.optimize import curve_fit
 
 df = pandas.read_csv('TGS2611_TGS2600_Calibration.csv', delimiter= ';')
 df.set_axis(['Year', 'Month', 'Day', 'Hour', 'Minute', 'Second', 'TGS2611', 'TGS2600', 'R0', 'T', 'RH', 'Vcc', 'CH4'], axis = 'columns', inplace = True)
-print(df['TGS2611'].tolist())
 
-def exp_func(x, a, b,c):
+def exp_func(x, a, b, c):
 	return a * np.exp(-b * x) + c
 
 def lin_func(x, a, b):
 	return a * np.array(x) + b
 
-x_data_humidity = df['R0'].tolist()
+def lin_func_2(x1, x2, a, b, c):
+	return a * np.array(x1) + b * np.array(x2) + c
+
+def power_func(x1, x2, a, b, c, h, n ):
+	return a * np.power(x1, h) + b * np.power(x2, n) + c
+
+x_data_humidity = df['RH'].tolist()
+x_data_temperature = df['T'].tolist()
 y_data_v = df['Vcc'].tolist()
-plt.plot(x_data_humidity, y_data_v, 'b-', label='data')
+plt.plot(x_data_humidity, y_data_v, 'o', linestyle = 'None', label='data')
+plt.xlabel('Humidity')
+plt.ylabel('V')
+res = stats.linregress(x_data_humidity, y_data_v)
+print([res.slope,  res.intercept])
+print(f"R-squared: {res.rvalue**2:.6f}")
+plt.plot(x_data_humidity, lin_func(x_data_humidity, res.slope,  res.intercept), 'r', label='fitted line')
+
+popt, pcov = curve_fit(lin_func, x_data_humidity, y_data_v)
+print(tuple(popt))
+plt.plot(x_data_humidity, lin_func(x_data_humidity, *popt), 'r-', label= 'fit: a = {:.5f}, b = {:.5f}'.format(tuple(popt)[0], tuple(popt)[1]))
+plt.legend()
 plt.show()
-# popt, pcov = curve_fit(lin_func, x_data_humidity, y_data_v)
-# print(popt)
-# plt.plot(x_data_humidity, lin_func(x_data_humidity, *popt), 'r-', label='fit: a=%5.3f, b=%5.3f' % tuple(popt))
-# plt.xlabel('Humidity')
-# plt.ylabel('V')
-# plt.legend()
-# plt.show()
