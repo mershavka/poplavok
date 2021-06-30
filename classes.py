57f0f1d2f3b94775f22a9cb513f0b44b1fb8effa
Анастасия Мершавка <mershavkaa@yandex.ru>
2021-06-30 20:26:35 +0300

    Выделение класса MeasurementServer

--- classes.py
+++ classes.py
@@ -1,6 +1,15 @@
+import enum
 from math import sqrt
 from scipy.optimize import curve_fit
 import numpy as np
+from enum import Enum
+from main import appendRowToCsv
+from driver import Driver
+import datetime as dt
+import time
+import csv
+import traceback
+from threading import Thread, Event
 
 class Model_fitting:
 
@@ -30,4 +39,110 @@ class Model_fitting:
 		self.adjusted_r_squared = 1 - (1-self.r_squared)*(len(self.y)-1)/(len(self.y)-self.predictors_count)
 		#Root Mean Square Error Среднеквадратическая ошибка модели
 		self.rmse = sqrt(self.ss_residual / len(self.y))
+
+class Status(enum.Enum):
+		NO = (0, "Nothing is happening")
+		ERROR = (1, "Error occured")
+		FIELD_EXPERIMENT = (2, "Callibrated experiment is underway")
+		CALIBRATION_1 = (3, "Experiment for calibration (first step) is underway")
+		CALIBRATION_2 = (4, "Experiment for calibration (second step) is underway")
+		WARNING = (5, "Warning")
+		def __init__(self, id, title):
+			self.id = id
+			self.title = title
+		def info(self):
+			print("Name - %s, id - %s, desription - %s"%(self.name, self.id, self.title))
+
+class Error(Exception):
+	"""Base class for other exceptions"""
+	pass
+
+class BoardConnectionError(Error):
+	pass
+
+class FilePathError(Error):
+	pass
 	
+class MeasurementServer:
+
+	def __new__(cls, data=None):
+		if data is None:
+			print('Error while creating class MeasurementServer')
+			return None
+		else:
+			return super().__new__(cls)
+
+	def __init__(self):
+		self.status = Status.NO
+		self.lastData = {}
+		self.event = Event()
+
+	def __runMeasurements(self, fileName, duration, periodicity, calibrationPath):
+		try:
+			expStart = time.time()
+			dr = Driver()
+			if (dr.open() < 0):
+				raise BoardConnectionError
+
+			appendRowToCsv(fileName, ['Time','ADC value','Voltage, V', 'Resistance, Om', 'Temperature,°C', 'Humidity, % RH', 'Pressure, hPa,', 'CH4, ppm'])
+			timing = 0
+
+			while time.time() - expStart <= duration:
+				if self.event.is_set():
+					break
+				if time.time() - timing >= periodicity:
+					timing = time.time()
+					dataDictionary = dr.readData()
+					newRow = ['{:%Y/%m/%d %H:%M:%S.%f}'.format(dataDictionary[Driver.timeString]), dataDictionary[Driver.adcString], \
+							round(dataDictionary[Driver.voltageString], 3), round(dataDictionary[Driver.resistanceString], 1), \
+							round(dataDictionary[Driver.temperatureString], 2), round(dataDictionary[Driver.humidityString], 2), round(dataDictionary[Driver.pressureString], 2)]
+					if calibrationPath != None:
+						#Здесь нужно считать результат калибровки и рассчитывать ppm метана
+						ch4 = 0
+						newRow.append(ch4)
+						print("CH4, ppm: {}".format(ch4), end= " ")
+					print("Time: {}, ADC value: {}, Voltage: {:.3f}, Resistance: {:.1f}, Temperature: {:.2f}, Humidity: {:.2f}, Pressure: {:.2f}".format(*newRow))
+					appendRowToCsv(fileName, newRow)
+			self.status = Status.NO
+		except BoardConnectionError:
+			print('Connection to Board FAILED!!! Please, check board connection with Raspberry Pi.')
+			self.status = Status.ERROR
+		except Exception as e:
+			traceback.print_exc()
+			self.status = Status.ERROR
+			return -1
+		finally:
+			dr = None
+		return 0
+
+	def __runExperiment(self, filePath, duration, periodicity, calibrationPath):
+		self.th = Thread(target=self.__runMeasurements, args=(filePath, duration, periodicity, calibrationPath))
+		self.th.start()
+
+		for i in range(5):
+			print(f"from main thread: {i}")
+			time.sleep(1)
+
+	def startExperiment(self, filePath=None, duration=600, periodicity = 2, calibrationPath = None):
+		self.event.clear()
+		if calibrationPath is None:
+			print('No calibration file')
+			self.status = Status.ERROR
+		else:
+			if filePath is None:
+				print('No file name for saving experiment')
+				filePath = ('exp_{:%Y_%m_%d_%H%M%S}.csv').format(dt.datetime.now())
+
+			self.status = Status.FIELD_EXPERIMENT
+			self.__runExperiment(filePath, duration, periodicity, calibrationPath)
+
+	def startCal1Experiment(self):
+		pass
+
+	def startCal2Experiment(self):
+		pass
+
+	def stopExperiment(self):
+		if self.th.is_alive:
+			self.event.set()
+		self.status = Status.NO
--- main.py
+++ main.py
@@ -10,45 +10,7 @@ def appendRowToCsv(filename, listOfElements):
 		writer = csv.writer(writeObj)
 		writer.writerow(listOfElements)
 
-def runMeasurements(file_name, duration, periodicity, calibrationPath=None):
-	try:
-		expStart = time.time()
-		dr = Driver()
-		if (dr.open() < 0):
-			print('Connection to Board FAILED!!! Please, check board connection with Raspberry Pi.')
-			raise Exception
 
-		appendRowToCsv(file_name, ['Time','ADC value','Voltage, V', 'Resistance, Om', 'Temperature,°C', 'Humidity, % RH', 'Pressure, hPa,', 'CH4, ppm'])
-		timing = 0
-
-		while time.time() - expStart <= duration:
-			if time.time() - timing >= periodicity:
-				timing = time.time()
-				dataDictionary = dr.readData()
-				newRow = ['{:%Y/%m/%d %H:%M:%S.%f}'.format(dataDictionary[Driver.timeString]), dataDictionary[Driver.adcString], \
-						round(dataDictionary[Driver.voltageString], 3), round(dataDictionary[Driver.resistanceString], 1), \
-						round(dataDictionary[Driver.temperatureString], 2), round(dataDictionary[Driver.humidityString], 2), round(dataDictionary[Driver.pressureString], 2)]
-				if calibrationPath != None:
-					ch4 = 0
-					newRow.append(ch4)
-					print("CH4, ppm: {}".format(ch4), end= " ")
-				print("Time: {}, ADC value: {}, Voltage: {:.3f}, Resistance: {:.1f}, Temperature: {:.2f}, Humidity: {:.2f}, Pressure: {:.2f}".format(*newRow))
-				appendRowToCsv(file_name, newRow)
-	except Exception as e:
-		traceback.print_exc()
-		return -1
-	finally:
-		dr = None
-	return 0
-
-def runExperiment(filePath, duration, periodicity, calibrationPath=None):
-	file_name = ('exp_{:%Y_%m_%d_%H%M%S}.csv').format(dt.datetime.now())
-	th = Thread(target=runMeasurements, args=(file_name, 1000, 1))
-	th.start()
-
-	for i in range(5):
-		print(f"from main thread: {i}")
-		time.sleep(1)
 
 def runExpCal1():
 	pass
