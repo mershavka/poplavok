71aa782123b51395c15103c365b3caf99227bf46
Анастасия Мершавка <mershavkaa@yandex.ru>
2022-06-10 14:23:11 +0300

    Добавление sandox с графиками результатов калибровки и скриптами для подготовки и усреднения данных

--- sandbox/bytes2double.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-double bytes2double(int high_byte, int low_byte)
-{
-	int bytes = high_byte * 256 + low_byte;
-	double value = (double)bytes;
-	printf("hb = %d lb = %d\n bytes = %d\n value = %f\n", high_byte, low_byte, bytes, value);	
-	return value;
-}
